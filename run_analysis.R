0ec39a0237fbd8581633dab895a77a7fe48d0f25
JimmyCodex <JimmyCodex@users.noreply.github.com>
2016-02-01 00:13:47 +0000

    Final analysis changes

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,22 @@
 library(plyr)
-
-setwd("/Users/alex/projects/data-science/data-cleaning/week-4/project/")
+library(reshape2)
 
 if(!file.exists("./data")) { dir.create("./data") }
 
 # File names
 
-file.clean <- "cleaned.txt"
+file.clean <- "./data/cleaned.txt"
 file.features <- "./data/features.txt"
 
 file.activities.lables <- "./data/activity_labels.txt"
 
 file.train.data <- "./data/train/X_train.txt"
 file.train.activities <- "./data/train/y_train.txt"
+file.train.subjects <- "./data/train/subject_train.txt"
 
 file.test.data <- "./data/test/X_test.txt"
 file.test.activities <- "./data/test/y_test.txt"
+file.test.subjects <- "./data/test/subject_test.txt"
 
 # Common variables
 
@@ -69,7 +70,9 @@ valid_columns <- function(columns_to_extract, features) {
 features.extract <- valid_columns(features.valid, load.file(file.features))
 features.extract.literals <- as.character((read.table(file.features)[features.extract,])$V2)
 
-features.extract.literals <- c(features.extract.literals, c("activities.numeric", "activities.literals"))
+# Add col names for both the numerical and the literal activities columns
+# We are specifically adding a column for each of them
+features.extract.literals <- c(features.extract.literals, c("activities.numeric", "activities", "subject"))
 
 activities.labels <- load.file(file.activities.lables)
 
@@ -80,7 +83,10 @@ test.activities.literals <- mapvalues(test.activities[,1],
                                       from=as.character(activities.labels[,1]), 
                                       to=as.character(activities.labels[,2]))
 test.features <- load.file(file.test.data, features.extract)
-test.data <- cbind(test.features, test.activities, test.activities.literals)
+
+test.subjects <- load.file(file.test.subjects)
+
+test.data <- cbind(test.features, test.activities, test.activities.literals, test.subjects)
 names(test.data) <- features.extract.literals
 
 ## Train
@@ -90,7 +96,22 @@ train.activities.literals <- mapvalues(train.activities[,1],
                                           from=as.character(activities.labels[,1]), 
                                           to=as.character(activities.labels[,2]))
 train.features <- load.file(file.train.data, features.extract)
-train.data <- cbind(train.features, train.activities, train.activities.literals)
+
+train.subjects <- load.file(file.train.subjects)
+
+train.data <- cbind(train.features, train.activities, train.activities.literals, train.subjects)
 names(train.data) <- features.extract.literals
+
 # Merge the training and the test sets to create one data set
 all.data <- rbind(train.data, test.data)
+
+# Perform analysis step 5
+debug.output(c("Melting data file", nrow(all.data)))
+all.data.subject_activity <- melt(all.data, id = c("subject", "activities"))
+
+debug.output(c("Dcasting data file", nrow(all.data)))
+all.data.final <- dcast(all.data.subject_activity, subject + activities ~ variable, mean)
+
+# Write the cleaned data to a file
+debug.output(c("Writing cleaned data file to ", file.clean))
+write.table(all.data.final, file.clean, row.names = FALSE, quote = FALSE)
